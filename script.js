0f57bb229656bb94d34b26127a8f399d14a94bbc
SahilBhure <79324481+SahilBhure@users.noreply.github.com>
2023-08-19 14:11:47 +0530

    Update script.js
    fixed some innerHTML

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textBar = document.getElementById('textbar');
 const button = document.getElementById('searchmovie');
-const showMovie = document.querySelector('.movie');
+const showMovie = document.querySelector('.movie-card');
+
 
 
 button.addEventListener('click',async() =>{
@@ -9,20 +10,25 @@ button.addEventListener('click',async() =>{
     const data = await res.json();
     //console.log(data.Search);
     MovieList(data);
+    
 });
 
 function MovieList(data){
-    let movieDiv = document.getElementsByClassName('movieDiv');
+    let movieDiv = document.getElementsByClassName('card_repo');
     if(movieDiv.length){
         showMovie.innerHTML = "";
     }else{
+        
         for(let i = 0; i < data.Search.length;i++){
-    //showMovie.innerHTML = `<div class = "Title">${data.Search[i].Title}</div>` 
-    showMovie.innerHTML += `
-    <div class ="movieDiv">
-     <div class = "movie-Name">
-      <h4>${data.Search[i].Title}</h4>
-     </div>
-    </div>`}
+        showMovie.innerHTML += `<div class="card_repo">
+        <div class="card-body-repo">
+                <div class="card-Img">
+            <img src="${data.Search[i].Poster}" alt="${data.name}" id="Imgpic">
+                </div>
+                <div class="card-title">${data.Search[i].Title}</div>
+                <div class="year">Year: ${data.Search[i].Year}</div>
+            </div>
+    </div>`
+        }
 }
 }
